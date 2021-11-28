6adb4edf1c78dc8cf0b4ce12bef6ddf6a6e85151
Petit Martin <martin.petit@etu.unilim.fr>
2021-11-28 16:28:26 +0100

    Add user_crud file, classified_crud file

---
+++ classified_crud.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>UE-3DW15</title>
+    </head>
+ 
+    <body>
+        <?php
+            include('classifieds_create.php');
+            include('classifieds_read.php');
+            include('classifieds_update.php');
+            include('classifieds_delete.php');
+         ?> 
+    </body>
+</html>
---
+++ user_crud.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>UE-3DW15</title>
+    </head>
+ 
+    <body>
+        <?php
+            include('users_create.php');
+            include('users_read.php');
+            include('users_update.php');
+            include('users_delete.php');
+         ?> 
+    </body>
+</html>
