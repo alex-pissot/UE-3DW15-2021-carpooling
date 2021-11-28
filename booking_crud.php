57354ddec41af335c9646519852f99780f59cb2f
Alex pissot <ylex@hotmail.fr>
2021-11-28 16:13:39 +0100

    Add booking_crud file + car_crud file

---
+++ booking_crud.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>UE-3DW15</title>
+    </head>
+ 
+    <body>
+        <?php
+            include('bookings_create.php');
+            include('bookings_read.php');
+            include('bookings_update.php');
+            include('bookings_delete.php');
+         ?> 
+    </body>
+</html>
---
+++ car_crud.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>UE-3DW15</title>
+    </head>
+ 
+    <body>
+        <?php
+            include('cars_create.php');
+            include('cars_read.php');
+            include('cars_update.php');
+            include('cars_delete.php');
+         ?> 
+    </body>
+</html>
