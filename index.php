14c5759d89a69ac636b84621ee2044e046c73ec9
Petit Martin <martin.petit@etu.unilim.fr>
2021-11-28 17:22:00 +0100

    Add index.php and css files

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        <title>Carpooling</title>
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet" >
+    </head>
+
+    <body>
+        
+        <div class="container main-content rounded">
+            <h1 class="title">Carpooling</h1>
+            <div class="row links">
+                <div class="col-3">
+                    <a class="btn btn-primary" href="user_crud.php">Traitement des utilisateurs</a>
+                </div>
+                <div class="col-3">
+                    <a class="btn btn-primary" href="car_crud.php">Traitement des voitures</a>
+                </div>
+                <div class="col-3">
+                    <a class="btn btn-primary" href="classified_crud.php">Traitement des annonces</a>
+                </div>
+                <div class="col-3">
+                    <a class="btn btn-primary" href="booking_crud.php">Traitement des réservations</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
+
